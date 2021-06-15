3ed4a8e0d5a18010d5bb3d06b0a781f66e1de9af
Vincent Russo <vincentrusso1@gmail.com>
2021-06-15 17:15:13 -0400

    Adding small changes to examples.

--- src/bb84.py
+++ src/bb84.py
@@ -17,8 +17,8 @@ def bb84_extended_nonlocal_game():
 
     pred_mat = np.zeros([dim, dim, num_alice_out, num_bob_out, num_alice_in, num_bob_in])
     pred_mat[:, :, 0, 0, 0, 0] = e_0 * e_0.conj().T
-    pred_mat[:, :, 0, 0, 1, 1] = e_p * e_p.conj().T
     pred_mat[:, :, 1, 1, 0, 0] = e_1 * e_1.conj().T
+    pred_mat[:, :, 0, 0, 1, 1] = e_p * e_p.conj().T
     pred_mat[:, :, 1, 1, 1, 1] = e_m * e_m.conj().T
 
     prob_mat = 1 / 2 * np.identity(2)
@@ -28,7 +28,7 @@ def bb84_extended_nonlocal_game():
 
 if __name__ == "__main__":
     prob_mat, pred_mat = bb84_extended_nonlocal_game()
-    enlg = ExtendedNonlocalGame(prob_mat, pred_mat, reps=1)
+    enlg = ExtendedNonlocalGame(prob_mat, pred_mat, reps=2)
 
     ent = enlg.quantum_value_lower_bound(iters=2, tol=10e-3)
     unent = enlg.unentangled_value()
--- src/chsh.py
+++ src/chsh.py
@@ -9,7 +9,8 @@ num_alice_inputs, num_alice_outputs = 2, 2
 num_bob_inputs, num_bob_outputs = 2, 2
 
 # The probability matrix and predicate matrix define any nonlocal game.
-prob_mat = np.array([[1 / 4, 1 / 4], [1 / 4, 1 / 4]])
+prob_mat = np.array([[1 / 4, 1 / 4],
+                     [1 / 4, 1 / 4]])
 pred_mat = np.zeros((num_alice_outputs, num_bob_outputs, num_alice_inputs, num_bob_inputs))
 
 # Populate predicate with "1" when winning condition is achieved and with zero
@@ -25,5 +26,6 @@ for a_alice in range(num_alice_outputs):
 chsh = NonlocalGame(prob_mat, pred_mat)
 
 # Calculate the quantum and classical value of the CHSH game.
-print("Quantum value: ", chsh.quantum_value_lower_bound())
+print("Quantum value (lower bound): ", chsh.quantum_value_lower_bound())
+print("Quantum value (upper bound): ", chsh.commuting_measurement_value_upper_bound())
 print("Classical value: ", chsh.classical_value())
